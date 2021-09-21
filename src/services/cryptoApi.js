e47cc07a9f1fc75ecba0cad91af8db3d7e7672dc
aznamle <namle1998@hotmail.com>
2021-09-20 19:18:52 -0700

    redux store added, homepage dynamic query added

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+//logic of fetching api
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '399b595699msha2229b021447d64p1a159djsndbbf792e8015'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
