29e8eeb50228c64a96e834aa07fc82dca7a5144c
aznamle <namle1998@hotmail.com>
2021-10-07 12:35:21 -0700

    added newsApi

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,7 +14,7 @@ const { Title } = Typography;
 
 const Homepage = () => {
 
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
 
